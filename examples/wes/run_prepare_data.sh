7d7a98194dbb4b723ca7fa53add2e5574accd201
Hans-Joachim Ruscheweyh <hansr@euler01.hpc-lca.ethz.ch>
2017-07-07 20:57:48 +0200

    deleted old files

--- examples/wes/run_prepare_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SNAKEMAKE=$1
-
-bsub -J prepare -n 1 -W 23:59 -R "rusage[mem=20000]" -e prepare_data.err -o prepare_data.out $SNAKEMAKE -s prepare_data.snake --cluster "bsub -eo {params.lsferrfile} -oo {params.lsfoutfile}" -j 100 -p -k
